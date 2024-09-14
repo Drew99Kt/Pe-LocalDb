af806269fbe4cbe8e04a24e3fbb10a9900683032
andrew <71528556+Drew99Kt@users.noreply.github.com>
2024-09-14 11:52:53 -0400

    Update Main.php
    some helpfull comments.

--- Pe-LocalDb/MyFirstPlugin/src/MyFirstPlugin/Main.php
+++ Pe-LocalDb/MyFirstPlugin/src/MyFirstPlugin/Main.php
@@ -20,10 +20,15 @@ class Main extends PluginBase implements Listener {
 
     
     public function onEnable(): void {
+        //set the cache in the current plugin
         $this->localDb = LocalDb::getCache();
+        //if you wanted you could set keys with the plugin name
         $this->pluginName = $this->getName();
+        //setting a key specific to the plugin
         $this->localDb->set( $this->pluginName, "example_key", "example_value");
+        //getting the plugin key
         $value =  $this->localDb->get( $this->pluginName, "example_key");
+        //printing in the console
         $this->getLogger()->info("Cached value: " . $value);
         $this->getLogger()->info(TextFormat::GREEN . "MyFirstPlugin has been enabled!");
         $this->getServer()->getPluginManager()->registerEvents($this, $this);
@@ -33,18 +38,23 @@ class Main extends PluginBase implements Listener {
         $this->getLogger()->info(TextFormat::RED . "MyFirstPlugin has been disabled!");
     }
     public function onBreak(BlockBreakEvent $event): void {
+        //get the player fromd the block break event
         $player = $event->getPlayer();
         //$player->sendMessage($event);
-      
+
+        //setting the last block broke by the player
         $this->localDb->set( "{$event->getPlayer()->getUniqueId()}-lastBlockBroke",  $event->getBlock()->getName());
-        
+        //get the count of blocks of this type the player has broken
         $blockBreakCount =  $this->localDb->get("{$event->getPlayer()->getUniqueId()}-brokeCount-{$event->getBlock()->getName()}");
+        //if this is the first time the player has broken this block then we need to set it to one
         if($blockBreakCount === null) {
             $blockBreakCount = 1;
         }
+        //setting the block broke counter in for this player
         $this->localDb->set( "{$event->getPlayer()->getUniqueId()}-brokeCount-{$event->getBlock()->getName()}",  $blockBreakCount+1);
+        //get the value of for last broken block
         $value =  $this->localDb->get( "{$event->getPlayer()->getUniqueId()}-lastBlockBroke");
-        
+        //send a message to the player letting them know what block they broke and the number of blocks of that type they have broken.
         $player->sendMessage(TextFormat::YELLOW . "The last block you broke was { $value}, you have broken { $blockBreakCount} of them");
     }
 
