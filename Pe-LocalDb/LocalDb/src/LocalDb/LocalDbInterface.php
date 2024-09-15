45140e909ac688fe1be59c024d6e0c44953ea3ba
andrew <71528556+Drew99Kt@users.noreply.github.com>
2024-09-15 09:49:13 -0400

    Update LocalDbInterface.php
    add player methods

--- Pe-LocalDb/LocalDb/src/LocalDb/LocalDbInterface.php
+++ Pe-LocalDb/LocalDb/src/LocalDb/LocalDbInterface.php
@@ -3,6 +3,9 @@ namespace LocalDb;
 
 interface LocalDbInterface {
     public function set(string $key, $value): void;
+    public function setByPlayerId(string $playerId,string $key, $value): void;
+    public function getByPlayerId(string $playerId,string $key);
+    public function deleteByPlayerId(string $playerId,string $key): void;
     public function get(string $key);
     public function delete(string $key): void;
     public function clear(): void;
